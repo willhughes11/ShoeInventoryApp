f247bdd2e0e39b0a9a2147dc877f5f90c9103330
willhughes11 <willhughes2110@gmail.com>
2020-12-27 21:26:55 -0500

    Changed root project folder

--- app/src/main/java/com/example/shoestore/screens/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/shoestore/screens/instructions/InstructionsFragment.kt
@@ -1,12 +1,17 @@
 package com.example.shoestore.screens.instructions
 
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.shoestore.R
+import com.example.shoestore.databinding.InstructionsFragmentBinding
 
 class InstructionsFragment : Fragment() {
 
@@ -19,8 +24,13 @@ class InstructionsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.instructions_fragment, container, false)
+    ): View {
+        val binding: InstructionsFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.instructions_fragment, container, false)
+        binding.instructionsNextButton.setOnClickListener {
+            findNavController().navigate(InstructionsFragmentDirections.actionInstructionsToShoeListings())
+        }
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/shoestore/screens/welcome/WelcomeFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.shoestore.R
+import com.example.shoestore.databinding.WelcomeFragmentBinding
 
 class WelcomeFragment : Fragment() {
 
@@ -19,8 +22,12 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.welcome_fragment, container, false)
+    ): View {
+        val binding: WelcomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.welcome_fragment, container, false)
+        binding.nextButton.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeToInstructions())
+        }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,5 +35,7 @@ class WelcomeFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(WelcomeViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    
 
 }
